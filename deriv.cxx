7da3e9fe970703e27305fdd70a3dd674c32a6e81
Michael Stumpf <mstumpf@pc03.pool.tp1.uni-duesseldorf.de>
2015-11-11 17:13:24 +0100

    lab4 done

--- deriv.cxx
+++ deriv.cxx
@@ -11,6 +11,22 @@ void print(double* p, const int N, const double dx,
        cout << i*dx + xmin << "\t" << p[i] << endl;
 }
 
+void gridpoints(double* p, const int N, const double xmin, const double dx){
+  for(int i=0; i<N; i++){
+    p[i] = exp(-(xmin+i*dx)*(xmin+i*dx));}
+  }
+  
+void derivative(double* p, const int N, const double dx){
+  double pb = p[0];
+  p[0]=p[1]/(2*dx);
+  
+  for(int i=1; i<N-1; i++){
+    double pbb = p[i];
+    p[i]=(p[i+1]-pb)/(2*dx);
+    pb = pbb;
+  }
+  p[N-1] = -pb/(2*dx);
+}
 
 int main(){
   const int N = 256;
@@ -18,9 +34,10 @@ int main(){
   const double xmax = 15;
   const double xmin = -15;
   const double dx = (xmax-xmin)/(N-1);
-
-  // call to function which fills array p here
-  // call to functio which calculates the derivative
+  
+  gridpoints(p, N, xmin, dx);	// call to function which fills array p here
+  derivative(p, N, dx);		// call to function which calculates the derivative
+  
   print(p,N,dx,xmin);
 
   return 0;
